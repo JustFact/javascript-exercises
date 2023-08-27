48f55fdb5b0b0bd47f17e86707e3a8234b7c8d5f
Jaspreet Singh <jaspreetsingh6881@gmail.com>
2023-08-27 20:13:33 +0530

    Complete exercise 04-removeFromArray...
    New concepts learnt-
    arguments Object: an array like object within a function that contains
    all the arguments of the function
    
    .find(<predicate>): returns the first element in the array that
    satisfy the <predicate> condition.
    
    .findIndex(<predicate>): returns the index of first element in the
    array that satisfy the <predicate> condition.
    
    predicate: A function that returns a boolean value based on the
    condition provided. Eg: e=>(e%2===0) //true for all even numbers e

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    for(let i = 1;i<arguments.length;i++){
+        if(arr.find(e=>e===arguments[i])){
+            let elementIndex = arr.findIndex(e=>(e===arguments[i]));;
+            arr.splice(elementIndex,1);
+        }    
+    }
+    return arr;
 };
 
 // Do not edit below this line
