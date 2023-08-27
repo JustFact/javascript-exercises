c78551707c41410cd07ef597174715bc2a12cc43
Jaspreet Singh <jaspreetsingh6881@gmail.com>
2023-08-27 21:20:29 +0530

    Complete exercise 05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(first,second) {
+    let sum = 0;
+    
+    if(first < 0 || second < 0 || typeof first != 'number' || typeof second != 'number'){
+        return 'ERROR'
+    } else if(first>second){
+        let temp = first;
+        first = second;
+        second = temp;
+    }
+    for(let i = first;i<=second;i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
