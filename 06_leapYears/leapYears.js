9158a6e76c1ddd4cf17638ad414bf30da42c35d5
Jaspreet Singh <jaspreetsingh6881@gmail.com>
2023-08-27 21:45:32 +0530

    Complete exercise 06-leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    if(year%100==0){
+        if(year%400==0){
+            return true;
+        }else{
+            return false;
+        }
+    }else if(year%4==0){
+        return true;
+    }else{
+        return false;
+    }
+    //Leap Years: divisible by 4
+    //Not Leap Years: divisible by 100, but divisible by 400 are leap year
 };
 
 // Do not edit below this line
