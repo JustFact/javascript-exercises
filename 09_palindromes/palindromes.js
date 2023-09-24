a025c71fdefbd3721f0013fd21383d1ad57aa3af
Jaspreet Singh <jaspreetsingh6881@gmail.com>
2023-09-24 21:35:16 +0530

    Complete 09 Palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let cleanArray = Array.from(str.toLowerCase())
+                        .filter(character=>character!=' ')
+                        .filter(character=>character!=',')
+                        .filter(character=>character!='!')
+                        .filter(character=>character!='.')
 
+    return cleanArray.join("")==cleanArray.reverse().join("");
+    // for(let i = 0; i < cleanArray.length; i++){
+    //     if(cleanArray[i]!=cleanArray[cleanArray.length-1-i]){
+    //         return false;
+    //     }
+    // }
+    // return true;
 };
 
 // Do not edit below this line
