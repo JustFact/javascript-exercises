7931f2f0b2bf62bb3f3b4bd6fa740c538ab98be9
Jaspreet Singh <jaspreetsingh6881@gmail.com>
2023-09-24 20:42:50 +0530

    Complete 08 Calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  arr.forEach(num => {
+    sum += num;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let multiple = 1;
+  arr.forEach(num=>{
+    multiple *= num;
+  })
+  return multiple;
 };
 
-const power = function() {
-	
+const power = function(num,pow) {
+  let multiple = num;
+	for(let i = 1; i < pow; i++){
+    multiple *= num;
+  }
+  return multiple;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  while(num){
+    result *= num;
+    num--;
+  }
+  return result;
 };
 
 // Do not edit below this line
