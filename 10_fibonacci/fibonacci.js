c499fb998190430ff0f6a851af7bd15d3268b202
Jaspreet Singh <jaspreetsingh6881@gmail.com>
2023-09-24 21:58:25 +0530

    Complete 10-Fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num<0){
+        return 'OOPS'
+    }
+    let adder = 0;
+    let currentNumber = 1;
+    let holdNum;
+    while(num-1){
+        holdNum = currentNumber;
+        currentNumber = currentNumber + adder;
+        adder = holdNum
+        num--;
+    }
+    return currentNumber;
 };
 
 // Do not edit below this line
